ca00cad4bdf61d8551ee6c48d9226bcb9919ec27
Gregor <zwickzwackzwieback@gmail.com>
2022-12-18 14:49:40 +0100

    chore: day09 setup

---
+++ src/day09/data/presents.ts
@@ -0,0 +1,48 @@
+import Present1 from "../assets/Present1.svg?component";
+import Present2 from "../assets/Present2.svg?component";
+import Present3 from "../assets/Present3.svg?component";
+import Present4 from "../assets/Present4.svg?component";
+import Present5 from "../assets/Present5.svg?component";
+
+export default [
+  {
+    id: 1,
+    importName: Present1,
+    dimensions: {
+      width: 60,
+      height: 60,
+    },
+  },
+  {
+    id: 2,
+    importName: Present2,
+    dimensions: {
+      width: 40,
+      height: 40,
+    },
+  },
+  {
+    id: 3,
+    importName: Present3,
+    dimensions: {
+      width: 70,
+      height: 60,
+    },
+  },
+  {
+    id: 4,
+    importName: Present4,
+    dimensions: {
+      width: 110,
+      height: 90,
+    },
+  },
+  {
+    id: 5,
+    importName: Present5,
+    dimensions: {
+      width: 90,
+      height: 70,
+    },
+  },
+];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import I18n from "./plugins/i18n";
-import App from "./day08/App.vue";
+import App from "./day09/App.vue";
 import "./tailwind.css";
 
 createApp(App).use(I18n).mount("#app");
