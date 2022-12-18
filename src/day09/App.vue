7ab5896ebaed66f7eac6343389c38b7c6501addb
Gregor <zwickzwackzwieback@gmail.com>
2022-12-18 14:49:35 +0100

    chore: day09 setup

--- src/day09/App.vue
+++ src/day09/App.vue
@@ -1,3 +1,15 @@
-<template>Hello</template>
+<template>
+  <div class="grid h-screen place-content-center">
+    <div class="max-w-md">
+      <img :src="treeUrl" />
+    </div>
+    <div class="flex items-end gap-4">
+      <component v-for="present in presents" :key="present.id" :is="present.importName" />
+    </div>
+  </div>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import treeUrl from "./assets/tree.svg?url";
+import presents from "./data/presents";
+</script>
