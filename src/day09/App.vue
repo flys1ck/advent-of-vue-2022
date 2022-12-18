90cd6d3b56d972891b5c2b36e1675e29f7b2dbaa
Gregor <zwickzwackzwieback@gmail.com>
2022-12-18 15:00:31 +0100

    feat: day09

--- src/day09/App.vue
+++ src/day09/App.vue
@@ -4,12 +4,26 @@
       <img :src="treeUrl" />
     </div>
     <div class="flex items-end gap-4">
-      <component v-for="present in presents" :key="present.id" :is="present.importName" />
+      <component v-for="present in computedPresents" :key="present.id" :is="present.importName" />
     </div>
+    <button class="mt-24 rounded border bg-slate-400 p-2" @click="isSortActive = !isSortActive">
+      Toggle Sort {{ isSortActive }}
+    </button>
   </div>
 </template>
 
 <script setup lang="ts">
 import treeUrl from "./assets/tree.svg?url";
 import presents from "./data/presents";
+
+const isSortActive = ref(false);
+const computedPresents = computed(() => (isSortActive.value ? getSortedPresents() : presents));
+
+const getSortedPresents = () => {
+  return [...presents].sort(
+    (presentA, presentB) =>
+      presentA.dimensions.height * presentA.dimensions.width -
+      presentB.dimensions.height * presentB.dimensions.width
+  );
+};
 </script>
